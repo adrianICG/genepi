1410a6330a0c2c59b81f6faf376ecc527e4ceda6
Adrian Campos <adrianC@qimrberghofer.edu.au>
2020-11-03 17:53:54 +1000

    added PRSice style output

--- PRSpipeline/CombineProfiles.py
+++ PRSpipeline/CombineProfiles.py
@@ -42,13 +42,13 @@ if args.PRSice:
 else:
 	cols2use=['IID','ALLELE_CT','SCORE1_SUM']
 
-PRSDF=pd.read_csv(firstFile,header=0,sep='\t',compression='infer',usecols=cols2use)
+PRSDF=pd.read_csv(firstFile,header=0,sep='\s+',compression='infer',usecols=cols2use)
 PRSDF.index=PRSDF.IID
 PRSDF.drop('IID',axis=1,inplace=True)
 PRSDF.drop('FID',axis=1,inplace=True)
 
 for f in scoreFiles:
-	tmpDF=pd.read_csv(f,header=0,sep='\t',compression='infer',usecols=cols2use)
+	tmpDF=pd.read_csv(f,header=0,sep='\s+',compression='infer',usecols=cols2use)
 	tmpDF.index=tmpDF.IID
 	tmpDF.drop('IID',axis=1,inplace=True)
 	tmpDF.drop('FID',axis=1,inplace=True)
