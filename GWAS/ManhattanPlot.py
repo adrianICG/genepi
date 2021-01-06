a44538379a7f37e6d4d7d76af0f369a992a32e68
Adrian Campos <adrianC@qimrberghofer.edu.au>
2021-01-06 13:08:13 +1000

    changed ticklabel sizes

--- GWAS/ManhattanPlot.py
+++ GWAS/ManhattanPlot.py
@@ -189,8 +189,11 @@ if not args.circular:
 		labels.append(str(int(chr)))
 	#Manhattan 2
 	topAx.set_xticks(medians) #add xticklabels
-	topAx.set_xticklabels(labels,fontsize=15)
+	topAx.set_xticklabels(labels,fontsize=20)
+	ylabels=topAx.get_yticklabels()
+	topAx.set_yticklabels(ylabels,fontsize=20)
 	topAx.set_ylabel('-log10(pvalue)',fontsize=30)
+	topAx.set_xlabel('Chromosome',fontsize=25)
 	topAx.set_title(args.T1,fontsize=15)
 	topAx.axhline(y=-np.log10(5e-8),color='red')
 	topAx.axhline(y=-np.log10(1e-5),linestyle='--',color='blue')
